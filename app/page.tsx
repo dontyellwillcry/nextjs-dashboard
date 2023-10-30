499a7e566eea73ccf79f6522773c627dbbc52c5e
LazyDotExe <papaporo@proton.me>
2023-10-30 13:38:17 -0500

    practicing adding font styling in page.tsx and layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import styles from './ui/home.module.css';
+import { lusitana } from '@/app/ui/fonts';
+
 
 
 import Link from 'next/link';
@@ -11,8 +13,13 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div className={styles.shape}></div>;
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <div className={styles.shape}></div>  {/*This is the module css form of styling. Notice it being imported up above. */}
+          <div
+          // This is the tailwind className format. Instead of the css. They both achive the same styling
+            className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+          />
+          {/* importing lusitana from the fonts.ts and applying them to the className */}
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
