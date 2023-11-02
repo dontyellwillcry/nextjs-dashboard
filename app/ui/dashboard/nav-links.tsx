cedf57d2d163e54781dced4650701825b33b45ac
LazyDotExe <papaporo@proton.me>
2023-11-01 20:36:16 -0500

    changed the 'a' tag to a Link tag

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -3,6 +3,8 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -22,14 +24,19 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          // the 'Link' tag used to be an 'a' tag (common link tag in html/React).
+          // Using the 'a' tag however would cause the whole dom to refresh and with Next.js
+          // We can make is smoother since the layout of the pages is the same we only want
+          // to refresh to pages content not the whole page. So we can use Link tag.
+          // Now when you click a link, only the content refreshes and not the whole page.
+          <Link
             key={link.name}
             href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
