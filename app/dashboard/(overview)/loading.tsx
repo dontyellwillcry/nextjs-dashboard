c45ef696b8cfeeb98c5a6e7e45a55f6b0b99334a
LazyDotExe <papaporo@proton.me>
2023-11-12 00:29:58 -0600

    Change the dashboard to use suspence?

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+/*Route groups allow you to organize files into logical groups without affecting the URL path structure. 
+When you create a new folder using parentheses (), the name won't be included in the URL path. 
+So /dashboard/(overview)/page.tsx becomes /dashboard.*/
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
